6b80f74d9c13e34ec97c51415d8a7864450a786e
jechlin <jechlin@adaptavist.com>
2024-04-02 07:37:23 +0100

    checkpoint

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, {useState} from 'react'
+
+interface Widget {
+    settings: Settings[],
+    code: string
+}
+
+const WidgetEditor: React.FC = () => {
+
+    const [widget, setWidget] = useState<Widget>({settings: [], code: ''})
+
+    return <div>
+        <pre style={{minHeight: 200}}>{JSON.stringify(widget, null, 2)}</pre>
+        <Settings
+            settings={widget.settings}
+            onChange={(settings) => setWidget({...widget, settings})}
+        />
+
+        <TextEditor
+            value={widget.code}
+            onChange={s => setWidget({...widget, code: s})}
+        />
+    </div>
+}
+
+type Settings = 'a' | 'b'
+
+interface SettingsProps {
+    settings: Settings[]
+    onChange: (settings: Settings[]) => void
+}
+
+const Settings: React.FC<SettingsProps> = ({settings, onChange}) => {
+    return <div>
+        {['a', 'b'].map(s => {
+            return <div key={s}>
+                <input id={s} type={'checkbox'} name={s} value={s} checked={settings.includes(s)}
+                       onChange={e => onChange(settings.includes(s) ? settings.filter(c => s !== c) : [...settings, s])}
+                />
+                <label htmlFor={s}>{s}</label>
+            </div>
+
+        })}
+    </div>
+}
+
+interface TextEditorProps {
+    value: string
+    onChange: (value: string) => void
+}
+
+const TextEditor: React.FC<TextEditorProps> = ({value, onChange}) => {
+    return <div>
+        <textarea
+            value={value}
+            onChange={(e) => onChange(e.target.value)}
+            className={"w-full h-64"}
+        />
+
+    </div>
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <WidgetEditor/>
+    )
 }
 
 export default App
